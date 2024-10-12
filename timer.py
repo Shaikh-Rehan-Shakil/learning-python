c02600650a435c813dfabf2df03fdbc2a35f5954
Shaikh-Rehan-Shakil <shaikhrehanshakil0625@gmail.com>
2024-10-13 02:32:20 +0530

    refactor(timer): added days to the clock..

--- timer.py
+++ timer.py
@@ -4,9 +4,10 @@ myTime = int(input("please enter time ins seconds: "))
 
 for x in range(myTime, 0, -1):
   seconds = x % 60
-  minutes = int(x / 60) % 60
-  hours = int(x / 3600)
-  print(f'{hours:02}:{minutes:02}:{seconds:02}')
+  minutes = (x // 60) % 60
+  hours = (x // 3600) % 24
+  days = x // 86400
+  print(f'{days:02}:{hours:02}:{minutes:02}:{seconds:02}')
 
   time.sleep(1)
 
