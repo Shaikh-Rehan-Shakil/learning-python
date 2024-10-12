1815c2a783ef04f162cad07ece9e0f76db106c31
Shaikh-Rehan-Shakil <shaikhrehanshakil0625@gmail.com>
2024-10-13 00:57:52 +0530

    initial commit: added basic calc script

---
+++ calc.py
@@ -0,0 +1,17 @@
+operator = input("select operation (+ - * /): ")
+
+num1 = float(input("please enter the first number: "))
+num2 = float(input("please enter the second number: "))
+
+if operator == "+":
+  print(round(num1 + num2))
+elif operator == "-":
+  print(round(num1 - num2))
+elif operator == "*":
+  print(round(num1 * num2))
+elif operator == "/":
+  print(round(num1 / num2))
+else :
+  print("please enter a valid number")
+
+
